7dfe8eed6f892af37360b05a447e81f786e8c80a
Andreea <andreea_popa22@yahoo.ro>
2022-05-04 20:33:23 +0300

    add people storage repository

--- SmartAgenda/src/main/java/Main.java
+++ SmartAgenda/src/main/java/Main.java
@@ -1,8 +1,12 @@
+import storage.PersonRepository;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello world");
+        System.out.println("~Clients and providers:");
+        PersonRepository personRepository = PersonRepository.getInstance();
+        System.out.println(personRepository.toString());
 
     }
 }
---
+++ SmartAgenda/src/main/java/storage/PersonRepository.java
@@ -0,0 +1,100 @@
+package storage;
+
+import entities.Client;
+import entities.Person;
+import entities.Provider;
+import entitiesHelpers.Gender;
+import lombok.Getter;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public class PersonRepository {
+    private List<Client> clients = new ArrayList<>();
+    private List<Provider> providers = new ArrayList<>();
+    private List<Person> people = new ArrayList<>();
+
+    private static PersonRepository person_repository_instance = null;
+
+    public static PersonRepository getInstance()
+    {
+        if (person_repository_instance == null)
+            person_repository_instance = new PersonRepository();
+
+        return person_repository_instance;
+    }
+
+    private PersonRepository()
+    {
+        Client client1 = Client.builder()
+                .clientId(1)
+                .name("Popescu Andra")
+                .age(30)
+                .email("andra.popescu23@yahoo.com")
+                .phone("0765456732")
+                .gender(Gender.FEMALE)
+                .build();
+        Client client2 = Client.builder()
+                .clientId(2)
+                .name("Popa Andrada")
+                .age(26)
+                .email("andrada.popa01@yahoo.com")
+                .phone("0765457692")
+                .gender(Gender.FEMALE)
+                .build();
+        Client client3 = Client.builder()
+                .clientId(3)
+                .name("Popovici Alex")
+                .age(30)
+                .email("alex.popovici13@yahoo.com")
+                .phone("0747626732")
+                .gender(Gender.MALE)
+                .build();
+        Provider provider1 = Provider.builder()
+                .id(1)
+                .name("Muresan Elena")
+                .age(35)
+                .email("elena-muresan@yahoo.com")
+                .phone("0765489632")
+                .gender(Gender.FEMALE)
+                .build();
+        Provider provider2 = Provider.builder()
+                .id(2)
+                .name("Marcov Elena")
+                .age(52)
+                .email("elena-marcov20@yahoo.com")
+                .phone("0765489987")
+                .gender(Gender.FEMALE)
+                .build();
+        clients.add(client1);
+        clients.add(client2);
+        clients.add(client3);
+        providers.add(provider1);
+        providers.add(provider2);
+        people.add(client1);
+        people.add(client2);
+        people.add(client3);
+        people.add(provider1);
+        people.add(provider2);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        for (Person person : this.people){
+            boolean isInstance = person instanceof Client;
+            if (isInstance) {
+                result.append("Client ");
+                result.append(((Client)person).getClientId());
+            }
+            else {
+                result.append("Provider ");
+                result.append(((Provider)person).getId());
+            }
+            result.append(" ");
+            result.append(person.getName());
+            result.append("\n");
+       }
+        return result.toString();
+    }
+}
